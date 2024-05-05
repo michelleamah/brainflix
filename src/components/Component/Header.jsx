96cb04a456dee65520c7f72151fa62dd9723455d
Michelle Mah <michellemah2001@gmail.com>
2024-05-05 02:48:32 -0400

    merge initial feature header into develop

---
+++ src/components/Component/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import avatar from '../../assets/images/Mohan-muruge.jpg'; 
+import logo from '../../BrainFlix-logo.svg'; 
+import upload from '../../assets/images/Icons/upload.svg';
+import './Header.scss'; 
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header__logo">
+        <img src={logo} alt="Logo" className="header__logo-image" />
+      </div>
+      <div className="header__flex-container">
+        <div className="header__search">
+            <input type="text" placeholder="Search" className="header__search-input" />
+        </div>
+        <div className="header__avatar">
+            <img src={avatar} alt="Avatar" className="header__avatar-image" />
+        </div>
+        </div>
+        <div className="header__upload">
+            <img src={upload} alt="Upload" className="header__upload-icon" />
+            <div className="header__upload-text">UPLOAD</div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
