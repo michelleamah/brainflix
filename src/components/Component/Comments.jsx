0f638d29489f21f5ba63beeaaca7a27da6899b81
Michelle Mah <michellemah2001@gmail.com>
2024-05-05 23:54:29 -0400

    Commit final develop changes

--- src/components/Component/Comments.jsx
+++ src/components/Component/Comments.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import avatar from '../../assets/images/Mohan-muruge.jpg'; 
-import submitComment from '../../assets/images/Icons/add_comment.svg';
-import './Comments.scss';
+import React from "react";
+import avatar from "../../assets/images/Mohan-muruge.jpg";
+import submitComment from "../../assets/images/Icons/add_comment.svg";
+import "./Comments.scss";
 
 function Comments({ comments }) {
   return (
@@ -9,32 +9,43 @@ function Comments({ comments }) {
       {comments.length} Comments
       <section className="comments">
         <div className="comments__form">
-        <div className="comments__format">
-          <img className="comments__form--avatar" src={avatar} alt="avatar" />
-        <div className="comments__form--flex">
-            <label htmlFor="commentText">JOIN THE CONVERSATION</label>
-            <form className="commentForm">
-              <textarea id="commentText" name="commentText" rows="5" placeholder="Add a new comment"></textarea>
-              <div className="commentButton">
-              <img src={submitComment} alt="Submit Comment" className="commentButton__icon" />
-              COMMENT
-              </div>
-            </form>
-          </div>
+          <div className="comments__format">
+            <img className="comments__form--avatar" src={avatar} alt="avatar" />
+            <div className="comments__form--flex">
+              <label htmlFor="commentText">JOIN THE CONVERSATION</label>
+              <form className="commentForm">
+                <textarea
+                  id="commentText"
+                  name="commentText"
+                  rows="5"
+                  placeholder="Add a new comment"
+                ></textarea>
+                <div className="commentButton">
+                  <img
+                    src={submitComment}
+                    alt="Submit Comment"
+                    className="commentButton__icon"
+                  />
+                  COMMENT
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </section>
       <section className="comments">
-        {comments.map(comment => (
+        {comments.map((comment) => (
           <div key={comment.id} className="comment">
-            <div className="comment-format">
-              <div className="comment-avatar"></div>
-              <div className="comment-flexbox">
-                <div className="comment-namedate">
-                  <p className="comment-name">{comment.name}</p>
-                  <p className="comment-date">{new Date(comment.timestamp).toLocaleDateString()}</p>
+            <div className="comment__format">
+              <div className="comment__avatar"></div>
+              <div className="comment__flexbox">
+                <div className="comment__namedate">
+                  <p className="comment__name">{comment.name}</p>
+                  <p className="comment__date">
+                    {new Date(comment.timestamp).toLocaleDateString()}
+                  </p>
                 </div>
-                <p className="comment-text">{comment.comment}</p>
+                <p className="comment__text">{comment.comment}</p>
               </div>
             </div>
           </div>
